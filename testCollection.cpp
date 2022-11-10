b6035b0ab52fe45d0dd5f27a46b13e4d95743613
David <89220527+the-nostromo-1@users.noreply.github.com>
2022-11-09 23:12:57 -0500

    update 11/09/22 23:12

--- testCollection.cpp
+++ testCollection.cpp
@@ -5,24 +5,30 @@
 #include "collection.hpp"
 #include <iostream>
 
-using std::cout; using std::cin; using std::endl;
-
+using std::cout;
+using std::cin;
+using std::endl;
 
 
 int main(){
 
-   int size=5;  // setting collection array size
+   int size = 5;  // setting collection array size
    double *a = new double[size]; // allocating dynamic array
 
    // initializing array
-   a[0] = 0.; a[1] = 10.; a[2] = 20.; a[3] = 30.; a[4] = 40.;
+   a[0] = 0.;
+   a[1] = 10.;
+   a[2] = 20.;
+   a[3] = 30.;
+   a[4] = 40.;
 
    cout << "Collection: ";
    output(a, size); // printing out the array
+   cout << endl;
 
    cout << "Collection value: " << value(a, size) << endl;
    
-   /*
+   
    // asking user to input a number
    cout << "Input number to find in the collection: ";
    double number;
@@ -35,22 +41,62 @@ int main(){
    else
       cout << "The number is at position " << index << endl;
 
-   */
-   /*
    // adding a new number to the collection
    addNumber(a, 55.5, size);
-   cout << "Collection after adding a new number: "; output(a, size);
-
+   cout << "Collection after adding a new number: ";
+   output(a, size);
+   /*
    // adding a duplicate number to collection
    // the function should not add it
    addNumber(a, 20., size);
-   cout << "Collection after adding existing number: "; output(a, size);
+   cout << "Collection after adding existing number: ";
+   output(a, size);
 
    // 
    removeNumber(a, 10., size);
-   cout << "Collection after removing number: "; output(a, size);
+   cout << "Collection after removing number: ";
+   output(a, size);
 
 
    delete [] a; // deallocating the array
    */
+}
+
+void output(const double *colPtr, const int size) {
+   for (int i = 0; i < size; ++i) {
+      cout << colPtr[i] << " ";
+   }
+}
+
+double value(const double *colPtr, const int size) {
+   double new_value;
+   for (int i = 0; i < size; ++i) {
+      new_value += colPtr[i];
+   }
+   return new_value;
+}
+
+int check(const double *colPtr, const double number, const int size) {
+   int temp_number;
+   for (int i = 0; i < size; ++i) {
+      if (colPtr[i] == number) {
+         return i;
+      }
+   }
+   return -1;
+}
+
+void addNumber(double *&colPtr, const double number, int &size) {
+   
+   int index_check = check(*&colPtr, number, size);
+   if (index_check == -1) {
+      ++size;
+      double *tmp = new double [size];
+      
+      for (int i = 0; i < size; ++i) {
+         tmp[i] = colPtr[i];
+      }
+      delete [] colPtr;
+      colPtr = tmp;
+   }
 }
